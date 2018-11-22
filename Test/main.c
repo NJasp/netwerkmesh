91b96ba807fd21a23157f8ed9f88b4bcf99ca770
ArnoLindeboom <alindeboom96@hotmail.com>
2018-11-22 04:43:09 -0800

    Change to while loop

--- Test/main.c
+++ Test/main.c
@@ -24,10 +24,13 @@
 
 int main(void)
 {
-    puts("Hello World!");
+    while(1)
+    {
+        puts("Hello World!");
 
-    printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
-    printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+        printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
+        printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+    }
 
     return 0;
 }
