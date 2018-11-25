d3e9185a3cd4ae40875dcec7659644d9e22290ed
Martijn <martijn_van_olst@hotmail.com>
2018-11-25 23:03:00 +0000

    Added SPI setup

--- Test/main.c
+++ Test/main.c
@@ -21,25 +21,32 @@
 
 #include <stdio.h>
 #include <mrf24j40.h>
+#include <mrf24j40_params.h>
 
 mrf24j40_t sender;
-const mrf24j40_params_t senderparams;
-spi_t sender_spi_struct;
+char *data = "xy";
 
 int main(void)
 {
     // senderparams vullen
     
 
-    mrf24j40_setup(&sender, &senderparams);
-    //mrf24j40_init(&sender);
+    mrf24j40_setup(&sender, mrf24j40_params);  
+    mrf24j40_reset(&sender);
+
+    //puts("Hallo Welt!\n");
+
+    //printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
+    //printf("This board features a(n) %s MCU.\n", RIOT_MCU);
 
     while(1)
     {
-        puts("Hallo Welt!\n");
 
-        printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
-        printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+    //mrf24j40_tx_prepare(&sender);
+    //mrf24j40_tx_load(&sender, (uint8_t*)data, 2, 0);
+
+    //mrf24j40_tx_exec(&sender);
+        //mrf24j40_reg_write_short(&sender, 0xf8f8, )
     }
 
     return 0;
