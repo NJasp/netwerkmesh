9fc753c56b0b1b0a31a32d3ee63eae038001dfa9
njasp <cat@national.shitposting.agency>
2018-11-28 14:33:40 +0100

    added code for testing receiving

--- Test/main.c
+++ Test/main.c
@@ -1,34 +1,24 @@
 #include <stdio.h>
 #include <mrf24j40.h>
+#include <mrf24j40_internal.h>
 #include <mrf24j40_params.h>
 
-mrf24j40_t sender;
 char *data = "xy";
 
 int main(void)
 {
-    mrf24j40_setup(&sender, mrf24j40_params);  
-    mrf24j40_reset(&sender);
+	mrf24j40_t dev;
+    mrf24j40_setup(&dev, mrf24j40_params);  
+    mrf24j40_reset(&dev);
 
-    //puts("Hallo Welt!\n");
+    while(1) {
+		uint8_t buf;
+		mrf24j40_rx_fifo_read(&dev, 0, &buf, 1);
 
-    //printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
-    //printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+		if (buf != 0) {
+			printf("\rreceived: %d:\t'%c'\n", buf, buf);
+		}
+	}
 
-    while(1)
-    {
-
-    //mrf24j40_tx_prepare(&sender);
-    //mrf24j40_tx_load(&sender, (uint8_t*)data, 2, 0);
-
-    //mrf24j40_tx_exec(&sender);
-        //mrf24j40_reg_write_short(&sender, 0xf8f8, )
-    }
-
-	// test board by setting random shit
-	//printf("mrf addr: %d\nmrf txpower: %d\n", mrf24j40_get_addr_short(dev), mrf24j40_get_txpower(dev));
-	//mrf24j40_set_addr_short(dev, 0x3);
-	//mrf24j40_set_txpower(dev, 100);
-	//printf("mrf addr: %d\nmrf txpower: %d\n", mrf24j40_get_addr_short(dev), mrf24j40_get_txpower(dev));
     return 0;
 }
