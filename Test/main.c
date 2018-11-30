e5233e9bef1b5b1fd17bee24b3b77a226545440f
Martijn <martijn_van_olst@hotmail.com>
2018-11-30 11:30:15 +0000

    Added minimal network stack and basic IPv6 address

--- Test/main.c
+++ Test/main.c
@@ -1,25 +1,51 @@
 #include <stdio.h>
 #include <mrf24j40.h>
-#include <mrf24j40_params.h>
 #include <mrf24j40_internal.h>
-//include <xtimer.h>
+#include <mrf24j40_params.h>
 
-mrf24j40_t sender;
-uint8_t data = 8;
+#include "msg.h"
+#include "net/ipv6/addr.h"
+#include "net/gnrc.h"
+#include "net/gnrc/netif.h"
+#include "net/gnrc/pktbuf.h"
 
 int main(void)
 {
-    mrf24j40_setup(&sender, mrf24j40_params);
-    mrf24j40_reset(&sender);
+    puts("RIOT network stack example application");
 
-    mrf24j40_tx_prepare(&sender);
+    /* get interfaces and print their addresses */
+    gnrc_netreg_entry_t server = GNRC_NETREG_ENTRY_INIT_PID(8888, sched_active_pid);
+    gnrc_netif_t *netif = NULL;
+    msg_t msg_queue[20];
+    msg_t msg;
 
-    while (1)
+    while ((netif = gnrc_netif_iter(netif)))
     {
-        mrf24j40_tx_load(&sender, &data, 1, 0);
+        ipv6_addr_t ipv6_addrs[GNRC_NETIF_IPV6_ADDRS_NUMOF];
+        int res = gnrc_netapi_get(netif->pid, NETOPT_IPV6_ADDR, 0, ipv6_addrs,
+                                  sizeof(ipv6_addrs));
 
-        mrf24j40_tx_exec(&sender);
+        if (res < 0)
+        {
+            continue;
+        }
+        for (unsigned i = 0; i < (unsigned)(res / sizeof(ipv6_addr_t)); i++)
+        {
+            char ipv6_addr[IPV6_ADDR_MAX_STR_LEN];
+
+            ipv6_addr_to_str(ipv6_addr, &ipv6_addrs[i], IPV6_ADDR_MAX_STR_LEN);
+            printf("My address is %s\n", ipv6_addr);
+        }
     }
 
+    msg_init_queue(msg_queue, 20);
+    gnrc_netreg_register(GNRC_NETTYPE_UDP, &server);
+
+    msg_receive(&msg);
+
+    gnrc_pktsnip_t *pkt = (gnrc_pktsnip_t *)msg.content.ptr;
+    gnrc_pktbuf_release(pkt);
+
+    /* main thread exits */
     return 0;
 }
