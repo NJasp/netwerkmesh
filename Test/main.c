b90526560a679bec0290a7f54ef7aa751a40fecc
Martijn <martijn_van_olst@hotmail.com>
2018-12-01 02:07:18 +0000

    Finished mrf24j40 receive code

--- Test/main.c
+++ Test/main.c
@@ -11,14 +11,17 @@
 #include "net/ipv6/addr.h"
 #include "net/gnrc.h"
 
-void request_ipv6_addresses_from_connected_devices(void);
+void request_ipv6_addresses_from_connected_devices(ipv6_addr_t * address);
+void incoming_data(gnrc_pktsnip_t * pkt);
 
 gnrc_netreg_entry_t mrf24j40;
+gnrc_pktsnip_t *pkt = NULL;
 mrf24j40_t sender;
+ipv6_addr_t src_ipv6_addr[GNRC_NETIF_IPV6_ADDRS_NUMOF];
 msg_t msg_queue[MQ_S];
 msg_t msg_to_send;
 msg_t msg_to_receive;
-char * data;
+char *data;
 
 int main(void)
 {
@@ -31,37 +34,45 @@ int main(void)
     gnrc_netreg_entry_init_pid(&mrf24j40, GNRC_NETREG_DEMUX_CTX_ALL, sched_active_pid);
     gnrc_netreg_register(GNRC_NETTYPE_IPV6, &mrf24j40);
 
-    request_ipv6_addresses_from_connected_devices();
+    request_ipv6_addresses_from_connected_devices(src_ipv6_addr);
 
     irq_disable();
 
-    data = "test123";
-    msg_to_send.type = 12;
-    msg_to_send.content.ptr = data;
-
-    msg_send_to_self(&msg_to_send);
-    //msg_queue_print();
-
-    msg_receive(&msg_to_receive);
-    printf("\r%s", (char*)msg_to_receive.content.ptr);
-
     while (1)
     {
-        //printf("\nhoihoi");
+        msg_receive(&msg_to_receive);
+        switch (msg_to_receive.type)
+        {
+        case GNRC_NETAPI_MSG_TYPE_RCV:
+            pkt = msg_to_receive.content.ptr;
+            incoming_data(pkt);
+            break;
+        case GNRC_NETAPI_MSG_TYPE_SND:
+            pkt = msg_to_receive.content.ptr;
+            //_handle_outgoing_pkt(pkt);
+            break;
+        case GNRC_NETAPI_MSG_TYPE_SET:
+        case GNRC_NETAPI_MSG_TYPE_GET:
+            msg_reply(&msg_to_receive, &msg_to_send);
+            break;
+        default:
+            break;
+        }
     }
 
     return 0;
 }
 
-void request_ipv6_addresses_from_connected_devices(void)
+void request_ipv6_addresses_from_connected_devices(ipv6_addr_t * address)
 {
     gnrc_netif_t *netif = NULL;
+    puts("hoi");
 
     while ((netif = gnrc_netif_iter(netif)))
     {
+        puts("hoi2");
         //Get addresses of all connected devices
-        ipv6_addr_t ipv6_addrs[GNRC_NETIF_IPV6_ADDRS_NUMOF];
-        int res = gnrc_netapi_get(netif->pid, NETOPT_IPV6_ADDR, 0, ipv6_addrs, sizeof(ipv6_addrs));
+        int res = gnrc_netapi_get(netif->pid, NETOPT_IPV6_ADDR, 0, address, sizeof((*address)));
 
         if (res < 0)
         {
@@ -71,8 +82,12 @@ void request_ipv6_addresses_from_connected_devices(void)
         {
             char ipv6_addr[IPV6_ADDR_MAX_STR_LEN];
 
-            ipv6_addr_to_str(ipv6_addr, &ipv6_addrs[i], IPV6_ADDR_MAX_STR_LEN);
+            ipv6_addr_to_str(ipv6_addr, &address[i], IPV6_ADDR_MAX_STR_LEN);
             printf("\rDevice found! Address is:  %s\n", ipv6_addr);
         }
     }
+}
+
+void incoming_data(gnrc_pktsnip_t * pkt) {
+    printf("\rData ontvangen: %s", (char*) pkt->data);
 }
