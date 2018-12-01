edcff52df6ddac23457bf11709026855ea2afd68
Martijn <martijn_van_olst@hotmail.com>
2018-12-01 00:53:56 +0000

    Working example of network stack and mrf24j40

--- Test/main.c
+++ Test/main.c
@@ -1,3 +1,5 @@
+#define MQ_S 32
+
 #include <stdio.h>
 
 #include <mrf24j40.h>
@@ -5,22 +7,61 @@
 #include <mrf24j40_params.h>
 
 #include "msg.h"
+#include "net/gnrc/netreg.h"
 #include "net/ipv6/addr.h"
 #include "net/gnrc.h"
-#include "net/gnrc/netif.h"
+
+void request_ipv6_addresses_from_connected_devices(void);
+
+gnrc_netreg_entry_t mrf24j40;
+mrf24j40_t sender;
+msg_t msg_queue[MQ_S];
+msg_t msg_to_send;
+msg_t msg_to_receive;
+char * data;
 
 int main(void)
 {
 
-    puts("RIOT network stack example application");
+    mrf24j40_setup(&sender, mrf24j40_params);
+    mrf24j40_reset(&sender);
 
-    /* get interfaces and print their addresses */
+    msg_init_queue(msg_queue, MQ_S);
+
+    gnrc_netreg_entry_init_pid(&mrf24j40, GNRC_NETREG_DEMUX_CTX_ALL, sched_active_pid);
+    gnrc_netreg_register(GNRC_NETTYPE_IPV6, &mrf24j40);
+
+    request_ipv6_addresses_from_connected_devices();
+
+    irq_disable();
+
+    data = "test123";
+    msg_to_send.type = 12;
+    msg_to_send.content.ptr = data;
+
+    msg_send_to_self(&msg_to_send);
+    //msg_queue_print();
+
+    msg_receive(&msg_to_receive);
+    printf("\r%s", (char*)msg_to_receive.content.ptr);
+
+    while (1)
+    {
+        //printf("\nhoihoi");
+    }
+
+    return 0;
+}
+
+void request_ipv6_addresses_from_connected_devices(void)
+{
     gnrc_netif_t *netif = NULL;
+
     while ((netif = gnrc_netif_iter(netif)))
     {
+        //Get addresses of all connected devices
         ipv6_addr_t ipv6_addrs[GNRC_NETIF_IPV6_ADDRS_NUMOF];
-        int res = gnrc_netapi_get(netif->pid, NETOPT_IPV6_ADDR, 0, ipv6_addrs,
-                                  sizeof(ipv6_addrs));
+        int res = gnrc_netapi_get(netif->pid, NETOPT_IPV6_ADDR, 0, ipv6_addrs, sizeof(ipv6_addrs));
 
         if (res < 0)
         {
@@ -31,10 +72,7 @@ int main(void)
             char ipv6_addr[IPV6_ADDR_MAX_STR_LEN];
 
             ipv6_addr_to_str(ipv6_addr, &ipv6_addrs[i], IPV6_ADDR_MAX_STR_LEN);
-            printf("My address is %s\n", ipv6_addr);
+            printf("\rDevice found! Address is:  %s\n", ipv6_addr);
         }
     }
-
-    /* main thread exits */
-    return 0;
 }
